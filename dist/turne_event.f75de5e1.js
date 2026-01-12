025dcd3f2d3ed12469e2e3b179b2929ae46e90bc
Alexander Henrique <henriquealexander60@gmail.com>
2026-01-11 21:14:39 -0300

    Criando conteúdo principal

--- dist/turne_event.f75de5e1.js
+++ dist/turne_event.f75de5e1.js
@@ -1,27 +1,22 @@
 AOS.init();
-
 const DataDoEvento = new Date("Feb 17, 2026 19:00:00");
 const timeStampDoEvento = DataDoEvento.getTime();
-
-const contaTime = setInterval( function() {
+const contaTime = setInterval(function() {
     const momentoAtual = new Date();
     const timeStampDoMomento = momentoAtual.getTime();
-
     const distanciaAteEvento = timeStampDoEvento - timeStampDoMomento;
-
-    const diasEmMs = 1000 * 60 *60 * 24;
-    const horasEmMs = 1000 * 60 *60;
-    const minutosEmMs = 1000 * 60;
-
+    const diasEmMs = 86400000;
+    const horasEmMs = 3600000;
+    const minutosEmMs = 60000;
     const diasAteOEvento = Math.floor(distanciaAteEvento / diasEmMs);
-    const horasAteOEvento = Math.floor((distanciaAteEvento % diasEmMs) / horasEmMs);
-    const minutosAteOEvento = Math.floor((distanciaAteEvento % horasEmMs) / minutosEmMs);
-    const segundosAteOEvento = Math.floor((distanciaAteEvento % minutosEmMs) / 1000);
-
+    const horasAteOEvento = Math.floor(distanciaAteEvento % diasEmMs / horasEmMs);
+    const minutosAteOEvento = Math.floor(distanciaAteEvento % horasEmMs / minutosEmMs);
+    const segundosAteOEvento = Math.floor(distanciaAteEvento % minutosEmMs / 1000);
     document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m e ${segundosAteOEvento}s`;
-
-    if (distanciaAteEvento < 0){
+    if (distanciaAteEvento < 0) {
         clearInterval(contaTime);
         document.getElementById('contador').innerHTML = 'Evento expirado';
     }
 }, 1000);
+
+//# sourceMappingURL=turne_event.f75de5e1.js.map
